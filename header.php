99a4dc592a9b6d4c0da5314010050a2a2c948e4b
CaptainSteredenn <63875289+CaptainSteredenn@users.noreply.github.com>
2020-04-18 20:15:22 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+
+
+
+<table align="center" width="700" style="padding:0;border-spacing:0;margin:0;">
+	<TR align="center">
+		<TD align="center" style="border:0;margin:0;padding:0">
+			<a href="index.php"><img src="/images/bandeau2.jpg" alt="TECH AND FUN" vspace="0" "></a>
+		</TD>
+	</TR>
+	<TR style="border:0;margin:0;padding:0">
+  		<TD bgcolor="#ffcc33" height="5" style="border:0;margin:0;padding:0"></TD>
+	</TR>
+</table>
+
